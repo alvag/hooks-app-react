a3d3ea5818e7cd4fdd0cac91e997dfe08837e8a6
Max Alva <malva@peruapps.com.pe>
2020-09-22 09:54:48 -0500

    useEffect

--- src/components/useEffect/Message.js
+++ src/components/useEffect/Message.js
@@ -1,17 +1,29 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Message = () => {
 
+    const [coords, setCoords] = useState({ x: 0, y: 0 });
+
     useEffect(() => {
-        console.log('componente montado');
-        return () => {
-            console.log('componente desmontado');
+
+        const mouseMove = (e) => {
+            const coords = { x: e.x, y: e.y };
+            setCoords(coords)
         }
-    }, [])
+
+        window.addEventListener('mousemove', mouseMove);
+
+        return () => {
+            window.removeEventListener('mousemove', mouseMove);
+        }
+    }, []);
 
     return (
         <div>
-            <h3>Message</h3>
+            <h3>Coords</h3>
+            <p>
+                x: {coords.x} y: {coords.y}
+            </p>
         </div>
     )
 }
